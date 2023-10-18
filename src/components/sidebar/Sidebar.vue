0371c3784490dbdef569ef45b72a7a970471994d
sichoi42 <42.4.sichoi@gmail.com>
2023-10-18 14:07:01 +0900

    initial commit

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+import Explorer from '@/components/sidebar/explorer/Explorer.vue';
+
+export default defineComponent({
+  components: {
+    Explorer,
+  },
+  props: {
+    width: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(props) {
+    const styleMap = computed(() => ({
+      width: `${props.width}px`,
+    }));
+
+    return {
+      styleMap,
+    };
+  },
+});
+</script>
+
+<template lang="pug">
+div(class="h-full" :style="styleMap")
+  component(is="Explorer")
+</template>
